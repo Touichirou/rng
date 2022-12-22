efd7da599cf570261ad19a136a932c9a37cb83fb
Horatio Caine <horatio.caine@hotmail.co.uk>
2022-12-22 14:57:29 +0900

    EDIT text displayed in result box.

--- javascript.js
+++ javascript.js
@@ -7,5 +7,6 @@ generate.addEventListener('click', function() {
     let userInput = maximum.value;
     if (userInput > 100 || userInput === "") {
         alert('100以下の数値を入力してください。') 
-    } else {generatedNumber.textContent = Math.floor(Math.random() * userInput) +1};
+    } else {
+        generatedNumber.textContent = '次の発表者は名簿順の' + (Math.floor(Math.random() * userInput) +1) + '番になります。'};
 });
